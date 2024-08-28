71a77211774df8a91c76db85c900c595f3b8400e
jesfustervi <jesfustervi@gmail.com>
2024-08-28 10:35:14 +0200

    adding tests for webkit

--- playwright.config.ts
+++ playwright.config.ts
@@ -47,9 +47,9 @@ export default defineConfig({
     //   use: { ...devices["Desktop Firefox"] },
     // },
 
-    // {
-    //   name: "webkit",
-    //   use: { ...devices["Desktop Safari"] },
-    // },
+    {
+      name: "webkit",
+      use: { ...devices["Desktop Safari"] },
+    },
   ],
 });
